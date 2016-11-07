b9359f32e9bde28101537188957f6206f22f2464
vetak <vetak@bmail.ru>
2016-11-07 19:04:37 +0300

    Пустая сцена. Добавлял и сортировал элементы.

---
+++ Assets/Scripts/EasyTimerDestroy/Timer.js
@@ -0,0 +1,16 @@
+﻿public var objectForDestroy : GameObject; // Объект , который будет уничтожаться.
+public var timeInSecond : float = 10.0; // время , по истечению которого, обьект , указанный выше уничтожиться.
+
+
+function Update ()
+{
+if (timeInSecond > 0)
+{
+timeInSecond -= Time.deltaTime;
+
+if (timeInSecond <= 0)
+{
+Destroy(objectForDestroy);
+}
+}
+}
